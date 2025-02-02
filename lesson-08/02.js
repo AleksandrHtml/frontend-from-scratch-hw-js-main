f56367e8a11612c3af249b068cd9c61a71163106
Aleksandr <aleksandrmaculevic@gmail.com>
2025-02-02 15:17:14 +0200

    complete lesson8-2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -33,11 +33,28 @@ let isTimerStarted = false
 let timerId
 
 startButton.addEventListener('click', () => {
-  let counter = 3
+  let counter = 3;
+  isTimerStarted = true;
+  countdownDisplay.textContent = counter;
+  timerId = setInterval(function() {
+    counter--;
+    countdownDisplay.textContent = counter;
+    if(counter === 0) {
+      countdownDisplay.textContent = "🚀";
+      clearInterval(timerId);
+      isTimerStarted = false;
+    }
+  }, 1000)
 
   // your code
 })
 
 cancelButton.addEventListener('click', () => {
   // your code
+  if(isTimerStarted) {
+    clearInterval(timerId);
+    countdownDisplay.textContent = "Отменено";
+    isTimerStarted = false;
+  }
+
 })
